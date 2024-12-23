22b7463d48708f4e40c5fd380154af668f78ef38
Arijit Chakraborty <arijit.chakraborty2304@gmail.com>
2024-12-23 18:38:56 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,16 +22,7 @@ function App() {
           />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
-          <Route
-            path="/dashboard"
-            element={
-              isAuthenticated ? (
-                <Dashboard />
-              ) : (
-                <Navigate to="/signup" replace />
-              )
-            }
-          />
+          <Route path="/dashboard" element={<Dashboard/>}/>
           <Route path="/share/:sharableLink" element={<SharePage />} />
         </Routes>
       </BrowserRouter>
